7d4c95366a3d739a5aa7f073e618c1b14a67741a
Min <solunar@vip.qq.com>
2023-05-03 21:26:29 +0800

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import '../app.css';
 </script>
 
-<slot />
+<div class="font-mono h-screen overflow-hidden flex">
+  <div class="bg-black text-white text-right w-48 p-5 flex flex-col">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+  </div>
+  <div class="flex-1 p-5">
+    <slot />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,1 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<h1>home</h1>
